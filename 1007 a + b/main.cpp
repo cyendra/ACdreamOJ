933cd5d121f4d974a6c1d5f83c227f8a431fe3b8
cyendra <g1483335912@gmail.com>
2014-05-19 12:45:12 +0800

    1007 fix and TLE

--- 1007 a + b/main.cpp
+++ 1007 a + b/main.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-
+#include <cstdio>
 using namespace std;
 typedef long long LL;
 const LL MOD = 10000000007;
 
 LL multi(LL a,LL b,LL m)
 {
-    LL ans = 0;
-    a %= m;
-    while(b)
-    {
-        if(b & 1)
-        {
-            ans = (ans + a) % m;
-            b--;
-        }
-        b >>= 1;
-        a = (a + a) % m;
+    LL ans=0;
+    a%=m;
+    b%=m;
+    while(b){
+        if(b & 1) ans=(ans+a)%m;
+        b>>=1;
+        a=(a+a)%m;
     }
     return ans;
 }
@@ -31,16 +27,20 @@ int main()
 {
     int T;
     LL n,k,A,ans;
-    cin>>T;
+    scanf("%d",&T);
+    //cin>>T;
     while (T--){
-        cin>>n>>k;
+        //cin>>n>>k;
+        scanf("%lld%lld",&n,&k);
         ans=0;
         for (int i=0;i<n;i++){
-            cin>>A;
+            //cin>>A;
+            scanf("%lld",&A);
             ans+=modPow(A,k,MOD);
             ans%=MOD;
         }
-        cout<<ans<<endl;
+        printf("%lld\n",ans);
+        //cout<<ans<<endl;
     }
     return 0;
 }
