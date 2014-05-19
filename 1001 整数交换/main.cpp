aeb2df17d76b12e735c2e7467013f4967af7a8ac
cyendra <g1483335912@gmail.com>
2014-05-19 11:34:24 +0800

    1000 1001

---
+++ 1001 整数交换/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+void SwapInt(int *a,int *b){
+    int t;
+    t=*a;
+    *a=*b;
+    *b=t;
+}
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
